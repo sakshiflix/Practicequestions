2e38cf32f38338365cd48a12dbdeebb9ba9501b2
Sakshiflix <sakshi01998@gmail.com>
2026-09-19 11:13:51 +0530

    Even or Odd

---
+++ question1.js
@@ -0,0 +1,12 @@
+// Even or Odd //
+
+// Write a program to check whether the number is even or odd.
+
+let number = 27;
+
+if (number % 2 === 0) {
+    console.log(number + " is an even number.");
+
+} else {
+    console.log(number + " is an odd number.");
+}
