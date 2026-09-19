bcae9c117da3a66484deb4fd0e2b48ae542706c9
Sakshiflix <sakshi01998@gmail.com>
2026-09-19 12:21:49 +0530

    Find Smallest Number

---
+++ question4.js
@@ -0,0 +1,9 @@
+// Find Smallest Number
+
+// Find the smallest number without using Math.min().
+
+let numbers = [25, 8, 42, 3, 19];
+
+let smallestNumber = Math.min(...numbers);
+
+console.log("Smallest number:", smallestNumber);
