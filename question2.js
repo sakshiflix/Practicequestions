2ea234c676f79fe4237fc1ccf3584dbcfe722c38
Sakshiflix <sakshi01998@gmail.com>
2026-09-19 12:09:14 +0530

    Count Characters

---
+++ question2.js
@@ -0,0 +1,10 @@
+// Sum of Array //
+
+//Find the sum of all elements using a loop.
+
+let numbers = [10, 20, 30, 40, 50];
+
+for (let i = 0; i < numbers.length;i++){
+    
+}
+
---
+++ question3.js
@@ -0,0 +1,19 @@
+// Count Characters
+
+let text = "javascript";
+
+// Print:
+
+//Length of the string
+
+console.log("Length of the string:", text.length);
+
+
+// First character
+
+console.log("First character:", text[0]);
+
+
+// Last character
+
+console.log("Last character:", text[text.length - 1]);
