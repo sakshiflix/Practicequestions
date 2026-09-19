272e5e080c37a624fa42a6ce9d7f8f7a0bac3671
Sakshiflix <sakshi01998@gmail.com>
2026-09-19 13:12:21 +0530

    sum of all elements using a loop

--- question2.js
+++ question2.js
@@ -4,7 +4,9 @@
 
 let numbers = [10, 20, 30, 40, 50];
 
-for (let i = 0; i < numbers.length;i++){
+let sum = 0
+for (let i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i]
     
 }
-
+console.log ("sum:", sum);
